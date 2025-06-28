7ccb94fe6196b5decfd95168f63909dbce8ef1df
sadman <ahsan.susmoy@gmail.com>
2025-06-28 09:41:06 +0600

    Login, Registration page  and custom redirect msg add with this.

--- IPCheckerApp/Controllers/HomeController.cs
+++ IPCheckerApp/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 using System.Diagnostics;
 using IPCheckerApp.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IPCheckerApp.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
---
+++ IPCheckerApp/Controllers/RedirectController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IPCheckerApp.Controllers
+{
+    public class RedirectController : Controller
+    {
+        public IActionResult ToLogin()
+        {
+            return Redirect("/Identity/Account/Login");
+        }
+    }
+}
